c2577c16858fb6038e9a1c0989de40919bc79a4b
RoshanGhadge20 <roshanghadge568@gmail.com>
2026-03-03 12:47:28 +0530

    Added first test for page title verification

---
+++ tests/homePage.spec.js
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test'
+
+// Test to verify page title 
+test('Verify Page Title', async ({ page }) =>
+{
+    page.goto("https://testautomationpractice.blogspot.com/");
+    const pageTitle = await page.title();
+    console.log("Page Title", pageTitle);
+    expect(page).toHaveTitle('Automation Testing Practice');
+})
