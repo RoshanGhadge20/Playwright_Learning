4ccca64f5435b509f6a68af853b7fcd1acb93874
RoshanGhadge20 <roshanghadge568@gmail.com>
2026-03-07 20:03:48 +0530

    Added dropdown testing file

---
+++ tests/dropDownElementHandling.spec.js
@@ -0,0 +1,46 @@
+import { test, expect } from '@playwright/test'
+import { waitForDebugger } from 'node:inspector';
+test("DropDown Handling", async ({ page }) => {
+    await page.goto('https://testautomationpractice.blogspot.com/', {
+        waitUntil: 'load'
+    });
+
+    // Approach 1 > with selecting the option with default options 
+    await page.locator('select#country').selectOption({ label: 'India' });
+    await page.locator('select#country').selectOption({ value: 'usa' });
+    await page.locator('select#country').selectOption('Germany');
+    await page.locator('select#country').selectOption({ index: 2 });
+
+    // Approach 2 > Directly we can call selectoption()
+    await page.selectOption('select#country', 'Japan');
+
+    // Assertions on dropdown Options 
+    const options = await page.$$('#country option');
+    await expect(options).toHaveCount(10);
+
+    // Assertion in form of array 
+    await expect(options.length).toBe(10);
+
+    // Check presence of value in dropdown with default methods
+    const content = await page.locator('#country').textContent();
+    await expect(content.includes('India')).toBeTruthy();
+
+    // with looping statements 
+    const optionElement = page.$$('#country option');
+
+    for (const opt of optionElement) {
+        // console.log("Element :- ", opt.textContent());
+        let value = await opt.textContent();
+        if (value.includes('France')) {
+            await opt.click();
+            break;
+        }
+    }
+
+
+
+    await page.waitForTimeout(2000);
+
+
+
+});
