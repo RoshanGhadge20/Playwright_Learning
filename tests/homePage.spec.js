396b206c39b8aa1fa79995287b816b21db0b9f60
RoshanGhadge20 <roshanghadge568@gmail.com>
2026-03-08 11:35:02 +0530

    Added few practice sets of testscripts in playwright with js

--- tests/homePage.spec.js
+++ tests/homePage.spec.js
@@ -1,8 +1,7 @@
 import { test, expect } from '@playwright/test'
 
 // Test to verify page title 
-test('Verify Page Title', async ({ page }) =>
-{
+test('Verify Page Title', async ({ page }) => {
     page.goto("https://testautomationpractice.blogspot.com/");
     const pageTitle = await page.title();
     console.log("Page Title", pageTitle);
---
+++ tests/multiSelectOptions.spec.js
@@ -0,0 +1,22 @@
+import { test, expect } from "@playwright/test";
+
+test("MultiSelect Dropdown Options", async ({ page }) => {
+    await page.goto("https://testautomationpractice.blogspot.com/", {
+        waitUntil: "load",
+    });
+
+    // Selecting the options from dropdown
+    await page.selectOption("select#colors", ["Blue", "Yellow"]);
+
+    // Assertions with default method
+    const options = await page.locator("select#colors options");
+    expect(options).toHaveCount(5);
+
+    // With the help of array elements
+    const option = await page.$$("select#colors options");
+    expect(option.length).toBe(5);
+
+    // Presence of Option in dropdown
+    const content = await page.locator("select#colors").textContent();
+    await expect(content.includes("Black")).toBeTruthy();
+});
